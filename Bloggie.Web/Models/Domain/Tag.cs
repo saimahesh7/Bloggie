2f83ff653608c7c5f5698cf51f9ad1a2acbe8efb
SAI MAHESH <saimaheshpanguluri7@gmail.com>
2024-03-15 14:26:32 +0530

    Before Creating Controllers and View pages

---
+++ Bloggie.Web/Models/Domain/Tag.cs
@@ -0,0 +1,12 @@
+﻿namespace Bloggie.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+        //Navigation Properties
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
